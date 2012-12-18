9ce4076dbb4abc38f217de7d588011b7642a1132
Naoki Yoshinaga <nap525@gmail.com>
2012-12-19 06:49:52 +0900

    First commit.

---
+++ src/main/java/com/napthats/android/evernote/EvernoteActivity.java
@@ -0,0 +1,14 @@
+package com.napthats.android.evernote;
+
+import android.app.Activity;
+
+import com.evernote.client.oauth.android.EvernoteSession;
+
+
+public class EvernoteActivity
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
