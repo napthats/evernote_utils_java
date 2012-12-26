60fbe4a9282c4d693b3b94558d1a20bb1b7d9b84
Naoki Yoshinaga <nap525@gmail.com>
2012-12-26 23:26:06 +0900

    Add createNoteStore returning NoteStoreProxy (tentative).

--- src/main/java/com/napthats/android/evernote/EvernoteActivity.java
+++ src/main/java/com/napthats/android/evernote/EvernoteActivity.java
@@ -1,13 +1,27 @@
 package com.napthats.android.evernote;
 
 import java.io.File;
+import java.util.List;
+
+import java.lang.reflect.Proxy;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
 
 import android.app.Activity;
 import android.os.Environment;
 import android.content.Context;
 import android.content.Intent;
+//
+import android.util.Log;
+//
 
 import com.evernote.client.oauth.android.EvernoteSession;
+import com.evernote.edam.type.Note;
+import com.evernote.edam.type.Tag;
+import com.evernote.edam.notestore.NoteStore;
+import com.evernote.edam.notestore.NoteList;
+import com.evernote.edam.notestore.NoteFilter;
 
 
 public class EvernoteActivity extends Activity
@@ -15,6 +29,64 @@ public class EvernoteActivity extends Activity
   private String EVERNOTE_HOST = "www.evernote.com";
 
   private EvernoteSession session = null;
+
+
+  //tentative
+  protected final NoteStoreProxy createNoteStore() {
+    checkInitialized();
+    return (NoteStoreProxy) Proxy.newProxyInstance(
+      NoteStoreProxy.class.getClassLoader(),
+      new Class[] {NoteStoreProxy.class},
+      new InvocationHandler() {
+        private NoteStore.Client note_store = null;
+        {
+          try { 
+            note_store = session.createNoteStore();
+          }
+          catch (Exception e) {
+            //TODO: Deal with exceptions.
+          }
+        }
+        public Object invoke(Object proxy, Method proxy_method, Object[] args)
+        throws Throwable {
+          //TODO: Deal with exceptions.
+          //TODO: Deal with few methods without an auth token.
+          try {
+            Object[] authkey_added_args = new Object[args.length + 1];
+            authkey_added_args[0] = session.getAuthToken();
+            System.arraycopy(args, 0, authkey_added_args, 1, args.length);
+            Class[] authkey_added_argsclass = new Class[args.length + 1];
+            authkey_added_argsclass[0] = String.class;
+            System.arraycopy(proxy_method.getParameterTypes(), 0, authkey_added_argsclass, 1, args.length);
+            Method method = note_store.getClass().getMethod(proxy_method.getName(), authkey_added_argsclass);
+            return method.invoke(note_store, authkey_added_args);
+          }
+          catch (Throwable e) {
+            throw e.getCause();
+          }
+        }
+      }
+    );
+  }
+
+    
+  public interface NoteStoreProxy {
+    public List<Tag> listTags();
+    public Note updateNote(Note note);
+    public Tag createTag(Tag tag);
+    public Note getNote(String guid, boolean b1, boolean b2, boolean b3, boolean b4);
+    public NoteList findNotes(NoteFilter filter, int i1, int i2);
+    public Note createNote(Note note);
+  }
+  
+
+  /**
+   * isLoggedIn.
+   */
+  protected final boolean isLoggedIn() {
+    checkInitialized();
+    return session.isLoggedIn();
+  }
 
 
   /**
